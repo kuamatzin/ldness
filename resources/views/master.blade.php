f1233c9f0924b2f5424ae2833f6494b74a32c132
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-01-31 12:54:57 -0600

    cambiar clases 10

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -37,7 +37,7 @@
                         </ul>
                     </div>
                 </nav>
-                <div id="nombre-seccion" class="nombre-seccion">
+                <div id="nombre-seccion" class="nombre-mobile-seccion">
                     @yield('name-section')
                 </div>
             </div>
