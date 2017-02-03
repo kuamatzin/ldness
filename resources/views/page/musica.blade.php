1b3e01885c71f6124821c97dd6d0e806097ef496
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-02-03 14:41:26 -0600

    Timgin

--- resources/views/page/musica.blade.php
+++ resources/views/page/musica.blade.php
@@ -20,7 +20,42 @@ class="background-musica background-class"
         </div>
     </div>
 </div>
+
 <img src="/img/discos.png" alt="" class="img-responsive">
+
+<div class="timing" id="intenta-div">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-6 timing-cont">
+            <h1 class="text-center">Descubre tu timing</h1>
+        </div>
+    </div>
+</div>
+
+
+<div class="container">
+    <div class="col-md-6 col-md-offset-3 timgin-div">
+        <h3>¿Qué es Timing?</h3>
+        <p>
+            La aceleración, desaceleración o detención tú de- cides qué timing ponerle a tu vida.
+        </p> 
+        <p>
+            Esta característica crea tu propio radio en base a lo que has escuchado. No importa qué tan rápido o lento vivas siempre tendrás tu propio timing. Muestra características de las personas, una canción que pueda cambiar el significado de las acciones, dar tiempo a que el espectador comprenda la situación, hacer que el espectador piense algo y luego cambiarlo con otra canción.
+        </p>
+        <h3>¿Cómo funciona?</h3>
+        <p>
+            Timing te recomienda la música en base a tus gustos musicales.
+        </p>
+        <h3>¿Quién puede utilizarlo?</h3>
+        <p>
+            Todos los usuarios pueden utilizarlo. Para los usuarios de teléfonos móviles Timing sólo estará disponible si tienes un dispositivo Android, iOS o Windows.
+        </p>
+        <p>
+            Los usuarios gratuitos sólo tendrán la opción de omitir 5 pistas a la hora.
+        </p>
+    </div>
+</div>
+
+
 <div class="intenta" id="intenta-div">
     <div class="row">
         <div class="col-md-4 col-md-offset-4 intenta-cont">
