f01bb07f10243f09387aa46dd2a449faeef00a19
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-02-08 14:02:08 -0600

    Cambios 10

--- resources/views/page/musico.blade.php
+++ resources/views/page/musico.blade.php
@@ -1,7 +1,7 @@
 @extends('master')
 
 @section('background-class')
-class="background-musico background-class"
+class="background-musico background-class-dos"
 @endsection
 @section('name-section')
 <div id="nombre-seccion" class="nombre-seccion-dos">
@@ -91,6 +91,7 @@ footer-background-musico
 @section('scripts')
 <script>
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        $('#background').removeClass( "background-mobile-class").addClass('.background-mobile-dos-class');
         $('#nombre-seccion').removeClass( "nombre-seccion-dos").addClass('nombre-mobile-seccion-dos');
     }
 </script>
